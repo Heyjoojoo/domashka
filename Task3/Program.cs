2e7e4347d56ae7e843c90e757fa3b3acaf42acac
Marina <tiffuncile@yandex.ru>
2022-10-02 15:14:14 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("Число " + n + " является чётным");
+}
+else
+{
+    Console.WriteLine("Число " + n + " является нечётным");
+}
