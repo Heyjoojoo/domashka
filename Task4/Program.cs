998594c64b43ff94aad37ff6434e65967db3711e
Marina <tiffuncile@yandex.ru>
2022-10-06 20:13:28 +0300

    Fixed Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i < n; i++)
-if (i % 2 == 0)
+for (int i = 2; i <= n; i+=2)
     Console.WriteLine (i + " ");
