ef64a22d3b437e6b4c47c32873c20b726416d84a
Marina <tiffuncile@yandex.ru>
2022-10-07 21:28:52 +0300

    Added Task3

---
+++ Seminar2/Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || n == 7)
+    Console.Write("Выходной, можем тусить");
+
+else 
+    Console.Write("нет :( ");
+
