5d0b395650839ce00fcfd47f24205b720e1e18c9
Marina <tiffuncile@yandex.ru>
2022-10-07 22:45:05 +0300

    Added lost seminar1

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите два числа: ");
+int n = Convert.ToInt32(Console.ReadLine ()); //после команды Convert.ToInt32 всегда возникает какая-то ошибка в файлах, они подсвечены красным и горят ошибки в папке obj
+int n2 = Convert.ToInt32(Console.ReadLine ());
+
+if (n > n2)
+{
+    Console.WriteLine(n + " больше, чем " + n2);
+}
+if (n < n2)
+{
+    Console.WriteLine(n2 + " больше, чем " + n);
+}
+if (n == n2)
+{
+    Console.WriteLine("Числа равны");
+}
