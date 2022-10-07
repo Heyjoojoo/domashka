ca1c7fe40758f39a465e6078ab7e80ae885cbc5b
Marina <tiffuncile@yandex.ru>
2022-10-07 21:21:14 +0300

    Added Task 2

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+
+if (n < 99)
+    Console.WriteLine("третьей цифры нет");
+
+else 
+{
+    while (n > 100)
+    {
+        m = n % 10;
+        n = n / 10;
+    
+    }
+    m = m % 10;
+    Console.WriteLine(m);
+}
+
