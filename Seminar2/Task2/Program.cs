48923faab1122c184c1ccce40cb9ff1d3897c58f
Marina <tiffuncile@yandex.ru>
2022-10-07 22:13:14 +0300

    Fixed Task2 with otricatelnie chisla lol

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -3,12 +3,12 @@ Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int m = 0;
 
-if (n < 99)
+if (n < 99 && n > -99)
     Console.WriteLine("третьей цифры нет");
 
 else 
 {
-    while (n > 100)
+    while (n > 100 || n < -100)
     {
         m = n % 10;
         n = n / 10;
