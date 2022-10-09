85d9bd0f2de379628f30a186d8a85fe8eaeb221e
Marina <tiffuncile@yandex.ru>
2022-10-09 18:40:59 +0300

    Commit Homework after 3d Seminar

---
+++ Seminar3/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000 || n > 100000)
+{
+    Console.WriteLine ("Это число не подходит, попробуйте снова: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n%10 == n/10000 && (n/10)%10 == (n/1000)%10)
+    Console.WriteLine("Это палиндром");
+else 
+    Console.WriteLine("Это не палиндром");
---
+++ Seminar3/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите количество кустов: ");
+int bush = Convert.ToInt32(Console.ReadLine());
+int [] berries = new int[bush];
+
+void FillArray(int[] berries)
+{
+    int bushindex = 0;
+    
+    while (bushindex < bush)
+    {
+        Console.WriteLine($"Введите количество ягод на кусте {bushindex+1}: ");
+        berries[bushindex] = Convert.ToInt32(Console.ReadLine());
+        bushindex++;
+    }
+}
+FillArray(berries);
+Console.WriteLine();
+
+Console.WriteLine("Введите номер куста, у которого находится манипулятор: ");
+int index = Convert.ToInt32(Console.ReadLine())-1;
+
+int TotalBerries = 0;
+
+if (index == 0)
+    TotalBerries = berries[index] + berries[bush-1] + berries[index+1];
+else if (index == bush-1)
+    TotalBerries = berries[index] + berries [index - 1] + berries[0];
+else
+    TotalBerries = berries[index] + berries [index - 1] + berries[index+1];
+ 
+Console.WriteLine ($"Силы небесные, Фрэнк! Здесь манипулятор соберёт аж {TotalBerries} ягод!");
