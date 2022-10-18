d99b3bfaee3e39b6d67c4fd09b7d347f85a44400
Marina <tiffuncile@yandex.ru>
2022-10-18 15:31:16 +0300

    Add homework to seminar 5

---
+++ Seminar5/Task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+void fillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(0, 10);
+    }
+}
+
+int sumArray (int [] array, int sum)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i%2 == 1)
+            sum = sum + array [i];
+            
+    }
+    return sum;
+}
+
+int [] array = new int [10];
+fillArray(array);
+Console.WriteLine($"Вот такой массив что ли [{string.Join(", ", array)}]");
+int sum = 0;
+Console.WriteLine($"{sumArray(array, sum)}");
+
---
+++ Seminar5/Task38/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// Внутренние функции использовать нельзя(Max и Min)
+// [3 7 22 2 78] -> 76
+
+void InputArray(double[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = Math.Round(new Random().NextDouble() * 10, 3);
+}
+
+double CountEventNumbers(double[] array)
+{
+double minArray = array[0], maxArray = array[0];
+for (int i = 1; i < array.Length; i++) // i = i + 2
+{
+if (maxArray < array[i])
+maxArray = array[i];
+else if (minArray > array[i])
+minArray = array[i];
+}
+return maxArray - minArray;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Разница между макс и мин: {CountEventNumbers(array)}");
+
+
+/*Console.Clear();
+
+void fillArray(double[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * 10, 3);
+    }
+}
+
+void findMax (double [] array, ref double nMax)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]>nMax)
+        nMax = array[i];
+    }
+}
+
+void findMin (double [] array, ref double nMin)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i]<nMin)
+        nMin = array [i];
+    }
+}
+
+double [] array = new double [5];
+fillArray(array);
+Console.WriteLine($"Вот такой массив что ли [{string.Join(", ", array)}]");
+double nMax = array[0];
+double nMin = array[0];
+findMax(array, ref nMax);
+findMin(array, ref nMin);
+Console.WriteLine($"Разница между {nMax} и {nMin} = {nMax-nMin}");
+*/
