ab64600cfc6af7d09693220b036092dc472825f4
Marina <tiffuncile@yandex.ru>
2022-10-22 16:05:58 +0300

    Add homework to seminar6

---
+++ Seminar6/Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+
+for (int i = 0; i < m; i++)
+{
+    Console.Write("Введите число: ");
+    int p = Convert.ToInt32(Console.ReadLine());
+    if (p>0)
+        count++;
+}
+
+Console.WriteLine(count);
---
+++ Seminar6/Task43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+x = (b2-b1)/(k1-k2);
+
+y = k1*x + b1;
+
+Console.WriteLine ($"{x}, {y}");
+
